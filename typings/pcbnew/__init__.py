55fd188651690cbf73566d638d695e3c4b135e6f
Mark K Cowan <mark@battlesnake.co.uk>
2023-12-27 14:36:55 +0200

    removed pcbnew v6 types

--- typings/pcbnew/__init__.py
+++
@@ -1,7 +0,0 @@
-# WARNING: pcbnew v6 API!  Quite outdated, we updated to v7.99 for the plugins now
-
-from eda_item import EDA_ITEM
-from board_item import BOARD_ITEM
-from board_item_container import BOARD_ITEM_CONTAINER
-from board import BOARD
-from footprint import FOOTPRINT
--- typings/pcbnew/footprint.py
+++
@@ -1,5 +0,0 @@
-from board_item_container import BOARD_ITEM_CONTAINER
-
-
-class FOOTPRINT(BOARD_ITEM_CONTAINER):
-    ...
